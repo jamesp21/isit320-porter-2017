32ae1be4b54910267068e6c367a5e7b6a11d25fa
james at school <noone@nowhere.net>
2017-09-20 16:39:49 -0700

    Working version of react basics

---
+++ Week01-ReactBasics/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactSimple from './react-simple.js';
+
+ReactDOM.render(
+  <ReactSimple/>,
+  document.getElementById('root')
+);
