96c22b5977ea59a4885f47c7212c9e79bf3de648
James Porter <porterjames1993@yahoo.com>
2017-12-06 19:48:23 -0800

    update

--- Week10-FireBaseReact/routes/index.js
+++ Week10-FireBaseReact/routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) { 'use strict';
   res.render('index', { title: 'Week10-FireBaseReact' });
 });
 
+router.get('/get-config', function(req, res, next) { 'use strict';
+    try {
+        config.useLocalConfig = false;
+        config.loadAsync()
+            .then(function (configuration) {
+                res.send({ configuration: configuration });
+            })
+            .catch(function (err) {
+                throw err
+            })
+    } catch(e) {
+        throw new Error(e);
+    }
+});
+
 module.exports = router;
